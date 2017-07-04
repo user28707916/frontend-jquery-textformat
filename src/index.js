a9a6be41510b70574586cfdbeffcf63e483b1b0c
user28707916 <kelen.omnel1@gmail.com>
2017-07-04 12:12:50 +0300

    Completed the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log($)
+var messageField = $('main .message-field');
+var messageFormatted = $('main .message-formatted');
+var textChange = $('main .message-form');
+
+String.prototype.trimAll = function formattedText() {
+    var r = /\s+/g;
+    return this.replace(r, '')
+        .toLowerCase();
+}
+
+function inputFormated() {
+    messageFormatted.text('');
+}
+
+messageField.on('clear', formattedText);
+textChange.on('input', inputFormated);
