247acaddb21552cc5f595c03330e1bc6f10fc139
user28707916 <kelen.omnel1@gmail.com>
2017-07-22 15:48:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-var messageField = $('.message-form .message-field');
-var messageFormatted = $('main .message-formatted');
+var messageField = $('.message-form > .message-field');
+var messageFormatted = $('main > .message-formatted');
+var textChange = $('main > .message-form'); 
 
-function formattedText(){
-     messageFormatted.text(
-         messageField.val().trim()
-                        .replace(/\s+/g, ' ')
-                        .toLowerCase()
+function formattedText() {
+    messageFormatted.text(
+        messageField.val().trim()
+            .replace(/\s +/g, ' ')
+            .toLowerCase()
     );
 }
 
-messageField.on('input', formattedText);
+function inputFormated(){
+    messageFormatted.text(' ');
+}
 
+messageField.on('input', formattedText);
+textChange.on('reset',inputFormated); 
 
 
